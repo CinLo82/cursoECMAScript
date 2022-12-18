3f70ce777d543479f266421ff78b614473171d20
CinLo <cinlos3382@gmail.com>
2022-12-17 23:09:55 -0300

    es11: optional chaining; bigIt y nullish; Promise.allSettled; globalThis y matchAll

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,12 @@
+const users = {
+    cinlo: {
+        country: 'ARG'
+    },
+    ana: {
+        country: 'MX'
+    }
+}
+
+console.log(users.cinlo.country);
+//console.log(users.cinlo.age);
+console.log(users?.bebeloper?.country);
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 87347983247823789423n;
+const anotherBigNumber = BigInt(87347983247823789423);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
